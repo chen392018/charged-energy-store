0d7c81d85d476459b4127e3c80df3e204273f69b
jmoffatt32 <jmoffatt32@gmail.com>
2023-12-02 12:21:23 -0500

    Linting

--- app/research/page.tsx
+++ app/research/page.tsx
@@ -1,18 +1,18 @@
 export default function ResearchPage() {
   return (
     <>
-      <section className='my-12 sm:my-24 w-full'>
-        <div className='mx-[4rem]'>
-            <h1 className='text-4xl text-accent-200 mb-2'>Research</h1>
-            <p className='text-accent-700 w-1/2 border-t pt-4'>
-              At charge, we beleive that at the core of any good product is good research. That is 
-              why we place a heavy emphasis on constantly researching and developing our products. We 
-              want to ensure that our products are the best they can be, and that you are getting the 
-              highest quality ingredients.
-            </p>
+      <section className="my-12 sm:my-24 w-full">
+        <div className="mx-[4rem]">
+          <h1 className="text-4xl text-accent-200 mb-2">Research</h1>
+          <p className="text-accent-700 w-1/2 border-t pt-4">
+            At charge, we beleive that at the core of any good product is good
+            research. That is why we place a heavy emphasis on constantly
+            researching and developing our products. We want to ensure that our
+            products are the best they can be, and that you are getting the
+            highest quality ingredients.
+          </p>
         </div>
       </section>
     </>
-
   )
 }
