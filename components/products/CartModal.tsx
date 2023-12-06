e92ac48c3c5713f2b53b609b4b0f00e4353593e1
michaelbelete <it.michael.belete@gmail.com>
2023-12-06 05:30:59 -0900

    feat: add fade animation for cart modal.

--- components/products/CartModal.tsx
+++ components/products/CartModal.tsx
@@ -16,7 +16,7 @@ export default function CartModal({
   const cartModalRef = useRef<HTMLDivElement>(null)
   const modalOverlayRef = useRef<HTMLDivElement>(null)
   const path = usePathname()
-  const cartItems: number[] = []
+  const cartItems: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
   // Close modal wehn navigating between pages
   useEffect(() => {
@@ -27,6 +27,9 @@ export default function CartModal({
   }, [path])
 
   useEffect(() => {
+    // disable scroll when modal is opened
+    document.body.classList.toggle("overflow-hidden")
+
     // only add the event listener when the modal is opened
     if (!showCart) return
 
@@ -40,6 +43,7 @@ export default function CartModal({
         setShowCart(!showCart)
       }
     }
+
     modalOverlay?.addEventListener("click", handleClick)
     // clean up
     return () => modalOverlay?.removeEventListener("click", handleClick)
@@ -47,72 +51,73 @@ export default function CartModal({
 
   return (
     <>
-      {showCart && (
+      <div
+        ref={modalOverlayRef}
+        className={`fixed top-0 left-0 flex items-center justify-center overflow-hidden bg-black/70 px-8 z-50 ${
+          showCart ? "w-screen h-screen" : "delay-200 w-0 h-0"
+        }`}
+      >
         <div
-          ref={modalOverlayRef}
-          className="fixed top-0 left-0 flex items-center justify-center w-screen h-screen bg-black/70 px-8 z-50"
+          ref={cartModalRef}
+          className={`w-full max-w-4xl p-4 space-y-8 backdrop-blur-xl rounded border border-primary-200 text-primary-100 transition-all ease-in-out duration-300 ${
+            showCart
+              ? "translate-y-0 opacity-100"
+              : "translate-y-full opacity-100"
+          }`}
         >
-          <div
-            ref={cartModalRef}
-            className="w-full max-w-4xl p-4 space-y-8 backdrop-blur-xl rounded border border-primary-200 text-primary-100"
-          >
-            <button
-              onClick={() => setShowCart(false)}
-              className="block ml-auto"
-            >
-              <MdClose className="duration-300 hover:rotate-180 w-8 h-8 md:w-10 md:h-10" />
-            </button>
-            {/* cart items */}
-            <div className="overflow-auto scrollbar-style">
-              {cartItems.length > 0 ? (
-                <ul className="max-h-[300px] px-4">
-                  {cartItems.map((item, index) => (
-                    <li
-                      key={item}
-                      className={`${
-                        index !== cartItems.length - 1 && "border-b"
-                      } border-secondary-700`}
-                    >
-                      <CartItem />
-                    </li>
-                  ))}
-                </ul>
-              ) : (
-                <div className="flex flex-col items-center justify-center gap-8">
-                  <p className="text-xl md:text-2xl">Your cart is empty!</p>
-                  <Link
-                    onClick={() => setShowCart(false)}
-                    href="/products"
-                    className="action-btn-style"
+          <button onClick={() => setShowCart(false)} className="block ml-auto">
+            <MdClose className="duration-300 hover:rotate-180 w-8 h-8 md:w-10 md:h-10" />
+          </button>
+          {/* cart items */}
+          <div className="overflow-auto scrollbar-style">
+            {cartItems.length > 0 ? (
+              <ul className="max-h-[300px] px-4">
+                {cartItems.map((item, index) => (
+                  <li
+                    key={item}
+                    className={`${
+                      index !== cartItems.length - 1 && "border-b"
+                    } border-secondary-700`}
                   >
-                    Continue Shopping
-                  </Link>
-                </div>
-              )}
-            </div>
-            {/* total */}
-            <div className="space-y-4 py-4 border-t border-secondary-400 text-sm md:text-lg">
-              <p className="flex justify-between">
-                <span className="uppercase">Subtotal</span>
-                <span>$9.99</span>
-              </p>
-
-              <p className="flex justify-between uppercase ">
-                <span>Shipping</span>
-                <span>Free</span>
-              </p>
-            </div>
-
-            {/* Checkout btn */}
-            <button
-              disabled={cartItems.length === 0}
-              className="action-btn-style w-full"
-            >
-              Proceed to checkout
-            </button>
+                    <CartItem />
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <div className="flex flex-col items-center justify-center gap-8">
+                <p className="text-xl md:text-2xl">Your cart is empty!</p>
+                <Link
+                  onClick={() => setShowCart(false)}
+                  href="/products"
+                  className="action-btn-style"
+                >
+                  Continue Shopping
+                </Link>
+              </div>
+            )}
           </div>
+          {/* total */}
+          <div className="space-y-4 py-4 border-t border-secondary-400 text-sm md:text-lg">
+            <p className="flex justify-between">
+              <span className="uppercase">Subtotal</span>
+              <span>$9.99</span>
+            </p>
+
+            <p className="flex justify-between uppercase ">
+              <span>Shipping</span>
+              <span>Free</span>
+            </p>
+          </div>
+
+          {/* Checkout btn */}
+          <button
+            disabled={cartItems.length === 0}
+            className="action-btn-style w-full"
+          >
+            Proceed to checkout
+          </button>
         </div>
-      )}
+      </div>
     </>
   )
 }
